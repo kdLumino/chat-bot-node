01149f853bf1826a452956631ed8ba86083f7d71
kdLumino <kuldeep.luminoguru@gmail.com>
2018-12-06 16:15:16 +0530

    debug

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 // Creates the endpoint for our webhook 
-app.post('/webhooks', (req, res) => {
+app.post('/webhook', (req, res) => {
 
     let body = req.body;
 
@@ -40,7 +40,6 @@ app.post('/webhooks', (req, res) => {
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
 
-    console.log("fgfgf");
     // Your verify token. Should be a random string.
     let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
 
